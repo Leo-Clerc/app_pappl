be8f477be3e1f12fedfcb92a14ff32a347d21374
Leo-Clerc <114158809+Leo-Clerc@users.noreply.github.com>
2022-11-17 16:00:14 +0100

    Ajout des gifs ?

--- EcranDeFinDePartie.js
+++ EcranDeFinDePartie.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import { Text, View, Button, Image } from 'react-native';
 
 
+
 const EcranDeFinDePartie = ({ route, navigation }) => {
   const gagne = route.params.gagne;  
   return(
         <View>
         <Text>Vous avez {gagne ? "gagné" : "perdu"}</Text>
-        <Image source={gagne ? require(".\winning") : require(".\sad")} />
+        <Image source={gagne ? require('./congrats.gif') : require('./kermit-worried.gif')} />
         <Button
         title="Revenir"
         onPress={() =>
