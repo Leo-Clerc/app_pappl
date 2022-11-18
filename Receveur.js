aae3dab84bf1197581384fe2acb770d3f8a82b6a
Leo-Clerc <114158809+Leo-Clerc@users.noreply.github.com>
2022-11-18 13:33:31 +0100

    AjoutImageEtRecceveur

---
+++ Receveur.js
@@ -0,0 +1,7 @@
+import {Image} from 'react-native'
+const AGE_SAHA = 47;
+const SEQ_ADN = "MYHKL"
+const NOM_RECEVEUR = ""
+const Receveur = () => {
+    return <Image source={require('./docteur.png')}>
+}
