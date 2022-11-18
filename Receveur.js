b8eb8cf1b4bd498d107f23316899733784cdbb14
Leo-Clerc <114158809+Leo-Clerc@users.noreply.github.com>
2022-11-18 17:32:45 +0100

    Ajout slider, toggle et champ textuel
    Le slider est à retravailler
    Le toggle également
    Le champ textuel doit venir visible et être typé
    Les booléens ne sont pas vérifiés correctemnt/Ne provoquent pas de render.

--- Receveur.js
+++ Receveur.js
@@ -1,8 +1,32 @@
-import {Image, View } from 'react-native'
+import React, { useState } from "react";
+import {Image, View, Switch, Text, TextInput } from 'react-native';
+import SliderAge from './SliderAge';
 const AGE_SAHA = 47;
 const SEQ_ADN = "MYHKL"
 const NOM_RECEVEUR = ""
+var res_slider = false;
+var res_txt = false
 const Receveur = () => {
-    return (<Image source={require('./docteur.png')}/>)
+    const [reponse, setReponse] = useState(false);
+  const toggleSwitch = () => setReponse(previousState => !previousState);
+    return( 
+    <View>
+        <Image source={require('./docteur.png')}/>
+        <Switch
+        onValueChange={toggleSwitch}
+        value={reponse}
+        />
+        <SliderAge 
+            ageCorrect={AGE_SAHA}
+            reponse = {res_slider}
+        />
+        <Text>Séquence de la protéine</Text>
+        <TextInput
+        onChange={(text) => {if(text==SEQ_ADN){res_txt = true};
+        }}/>
+        <Text>L'âge rentré est {res_slider ? "le bon" : "faux"}</Text>
+        <Text>La séquence rentrée est {res_txt ? "la bonne" : "fausse"}</Text>
+    </View>    
+    )
 }
 export default Receveur
---
+++ SliderAge.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import {Slider} from 'react-native';
+const SliderAge = ({ageCorrect, reponse}) => {
+    [ageEntre, setAgeEntre] = useState();
+    return <Slider
+            onValueChange = {(val)=> { setAgeEntre(val);
+                            if(val==ageCorrect){reponse = true}}
+            }
+            minimumValue = {18}
+            maximumValue = {80}
+            renderInsideCircle={(val) => <Text>{val}</Text>}
+
+            
+    />
+
+}
+export default SliderAge
