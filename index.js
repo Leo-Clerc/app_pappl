e58f32baf5b1222dbfe6faa4bbe3e87928f1337e
Leo-Clerc <114158809+Leo-Clerc@users.noreply.github.com>
2022-11-26 18:26:49 +0100

    App et site
    Reste à voir comment flasher le QR code...

--- index.js
+++ index.js
@@ -2,10 +2,11 @@
  * @format
  */
 
+import {AppRegistry} from 'react-native';
+import {name as appName} from './app.json';
 import {registerRootComponent} from 'expo';
 import App from './App';
-import Aide from './Aide';
-import ParametragePartie from './ParametragePartie';
+AppRegistry.registerComponent(appName, () => App);
 registerRootComponent(App);
 
 
