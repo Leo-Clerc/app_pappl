c68acd14ad5a24bf87aa964ca72ac1c348493fec
Leo-Clerc <114158809+Leo-Clerc@users.noreply.github.com>
2022-11-28 20:41:51 +0100

    La ça a l'air de faire ce que je veux

--- SliderAge.js
+++ SliderAge.js
@@ -3,19 +3,19 @@ import { useState } from 'react';
 import { Text, View } from 'react-native';
 import Slider from '@react-native-community/slider';
 
-const SliderAge = ({bonAge }) =>{
+const SliderAge = ({bonAge, correct}) =>{
     [age, setAge] = useState(20);
     return  <View>
     <Text>Âge rentré : {age}</Text>
     <Slider
-        value = {age}
+        value= {age}
         onValueChange={setAge}
         step = {1}
         minimumValue = {20}
         maximumValue = {80}
         
     />
-    <Text>La valeur rentrée est {(value==bonAge) ? "correct" : "incorrecte"}</Text>
+    <Text>La valeur rentrée est {(age==bonAge) ? "correct" : "incorrecte"}</Text>
 </View>
 }
 
