d52f9e6c7932aef804d9a19c171619fcd3932d71
Leo-Clerc <114158809+Leo-Clerc@users.noreply.github.com>
2022-11-29 14:43:57 +0100

    Plus de spoil de réponse du slider

--- ChampAge.js
+++ ChampAge.js
@@ -5,18 +5,20 @@ import Slider from '@react-native-community/slider';
 
 const ChampAge = ({bonAge, correct}) =>{
     [age, setAge] = useState(20);
+    [res, setRes] = useState(false);
     return  <View>
     <Text>Âge rentré : {age}</Text>
-    <TextInput value={age} />
+    <TextInput value={age} onChangeText = {setAge}/>
     <Slider
         value= {age}
         onValueChange={setAge}
+        onSlidingComplete={value => setRes(value==bonAge)}
         step = {1}
         minimumValue = {20}
         maximumValue = {80}
         
     />
-    <Text>La valeur rentrée est {(age==bonAge) ? "correct" : "incorrecte"}</Text>
+    <Text>La valeur rentrée est {res ? "correct" : "incorrecte"}</Text>
 </View>
 }
 
