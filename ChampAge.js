ffe42663f3e068fcf5debeb9d9eee08982eb1c44
Leo-Clerc <114158809+Leo-Clerc@users.noreply.github.com>
2022-11-29 16:47:55 +0100

    Je crois que ça marche pour que le parent récupère

--- ChampAge.js
+++ ChampAge.js
@@ -3,22 +3,21 @@ import { useState } from 'react';
 import { Text, View, TextInput } from 'react-native';
 import Slider from '@react-native-community/slider';
 
-const ChampAge = ({bonAge, correct}) =>{
+const ChampAge = ({bonAge, ageOk, setAgeOk ,changeAgeOk}) =>{
     [age, setAge] = useState(20);
-    [res, setRes] = useState(false);
     return  <View>
     <Text>Âge rentré : {age}</Text>
     <TextInput value={age} onEndEditing = {setAge} editable ={true}/>
     <Slider
         value= {age}
         onValueChange={setAge}
-        onSlidingComplete={value => setRes(value==bonAge)}
+        onSlidingComplete={value => changeAgeOk(value==bonAge)}
         step = {1}
         minimumValue = {20}
         maximumValue = {80}
         
     />
-    <Text>La valeur rentrée est {res ? "correcte" : "incorrecte"}</Text>
+    <Text>La valeur rentrée est {ageOk ? "correcte" : "incorrecte"}</Text>
 </View>
 }
 
