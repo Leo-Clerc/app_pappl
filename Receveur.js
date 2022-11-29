c04410e35c55502dee8249a806d60832713dc534
Leo-Clerc <114158809+Leo-Clerc@users.noreply.github.com>
2022-11-29 16:45:10 +0100

    Dernier état stable avant de tenter de mettre un call back dans ChampsAge

--- Receveur.js
+++ Receveur.js
@@ -1,18 +1,25 @@
 /**
  * @return Ce composant est l'interface à remplir par l'utilisateur sur l'écran de jeu 1
  */
-import { Image, Text, TextInput } from "react-native";
+import { Image, Text, TextInput, Button } from "react-native";
+import { useState } from "react";
 import React from 'react';
 import { View } from "react-native";
 import ChampAge from './ChampAge'
 let imgSrc = require('./docteur.png');
 const AgeDoc = 47;
-
+const seqProteine = "MYHKL"
 const Receveur = () => {
+    let [ageOk,setAgeOk] = useState(false)
+    let [proteineOk, setProteineOk] = useState(false)
+    const changeAgeOk = () => setAgeOk(prev => !prev);
     return <View>
-                <Text>Test pour le web</Text>
+                <Text>{ageOk ? "Oui" : "Non"}</Text>
                 <Image style={{width: 300, height: 400}} source={imgSrc}/>
-                <ChampAge bonAge={AgeDoc} correct = {false}/>
+                <ChampAge bonAge={AgeDoc} ageOk = {ageOk} changeAgeOk = {changeAgeOk}/>
+                <TextInput onEndEditing={value => setProteine(value==seqProteine)}/>
+                <Text>La valeur rentrée EST {ageOk ? "correcte" : "incorrecte"}</Text>
+                <>{ageOk&&<Text>Waw</Text>}</>
             </View>
 }
 export default Receveur
