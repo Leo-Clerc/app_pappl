28546ec37363134198de527dff786a8b427ed788
Leo-Clerc <114158809+Leo-Clerc@users.noreply.github.com>
2022-12-07 14:40:33 +0100

    En train d'essayer de résoudre le textInput
    Le problème est que c'est pas un number qui est passé en argument derrière...
    Là je vais faire de la doc, parce que ça me saoule

--- ChampAge.js
+++ ChampAge.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { useState } from 'react';
 import { Text, View, TextInput, Button } from 'react-native';
 import Slider from '@react-native-community/slider';
 
 const ChampAge = ({bonAge, age, changeAge, changeAgeOk}) =>{
+    [ageTI, setAgeTI] = useState(age)
     return  <View>
-    <TextInput style = {StyleSheet.input} value = {age} onChangeText = {(value) => {changeAge(value); changeAgeOk(value==bonAge)}} />
-    
-    
-    <Button title="Test" onPress ={()=>changeAge(77)}/>
+    <TextInput style = {StyleSheet.input} value = {ageTI.toString()} onChangeText = {(value) => {setAgeTI(value)}} onBlur = {(value) => {value ? changeAge(Number(value)) : changeAge(20); changeAgeOk(age == value)}} keyboardType="numeric"  />
+    <Slider
+        value= {age}
+        onValueChange={(value) => {changeAge(value); setAgeTI(value)}}
+        onSlidingComplete={(value) => changeAgeOk(value==bonAge)}
+        step = {1}
+        minimumValue = {20}
+        maximumValue = {80}
+    />
+    <Text>{typeof AgeTI}</Text>
 </View>
 }
 
