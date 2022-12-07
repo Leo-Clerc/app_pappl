a882cd94f213713f918dbb1ddd7b9791a8d96889
Leo-Clerc <114158809+Leo-Clerc@users.noreply.github.com>
2022-12-07 14:48:07 +0100

    doc aide

--- Aide.js
+++ Aide.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
 
-
+/**
+ * 
+ * @returns L'écran d'aide, contenant la documentation destinée à l'utilisateur.
+ */
 const Aide = () => {
     return(
         <Text>La section aide sera ici</Text>
