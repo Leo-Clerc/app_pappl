e81d1b43c36125500f545ae2ab53fe0b2f5b3c55
Leo-Clerc <114158809+Leo-Clerc@users.noreply.github.com>
2022-12-28 22:55:07 +0100

    Quelques debug sur la syntaxe de génération aléatoire, et début de la structure de donneurs

--- EcranDeJeu2.js
+++ EcranDeJeu2.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { Text, View, Button } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 const nomDonneurCorrect = "Dr Maya Curado";
 const ageDonneurCorrect = 36;
-const donneursPotentiels = [["Léon Patounec", 22],["Dr Emilia Caubert",65],["Thomas Parker",61],["Béatrice Tapalinga",23],["Vlad Koscov",55]];
+const donneursPotentiels = [["Léon Patounec", 22, "M"],["Dr Emilia Caubert",65, "F"],["Thomas Parker",61, "M"],["Béatrice Tapalinga",23, "F"],["Vlad Koscov",55,"M"]];
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
@@ -14,32 +15,28 @@ const positionDonneurCorrect = getRandomInt(4);
 let indexFauxDonneurs = [];
 while(indexFauxDonneurs.length<3){
   var nouvelIndex = getRandomInt(5)
-  if(indexFauxDonneur.indexOf(nouvelIndex)===-1){
+  if(indexFauxDonneurs.indexOf(nouvelIndex)===-1){
     indexFauxDonneurs.push(nouvelIndex)
   }
 }
-const fauxDonneurs = [donneursPotentiels]
+const Donneurs =  [];
+let donneurCorrectAjoute = false;
+for(index=0;index<indexFauxDonneurs.length+1;index++){
+  if(index==positionDonneurCorrect){
+    Donneurs.push([nomDonneurCorrect, ageDonneurCorrect,"F"])
+    donneurCorrectAjouté = true;
+  }
+  else if(!donneurCorrectAjoute){
+  Donneurs.push(donneursPotentiels[indexFauxDonneurs[index]])
+  }
+  else{Donneurs.push(donneursPotentiels[indexFauxDonneurs[index-1]])}
+}
 
 const EcranDeJeu2 = ({ navigation }) => {
     return(
         <View>
-        <Text>Le deuxième écran de jeu est là</Text>
-        <Button
-        title="Gagner"
-        onPress={() =>
-          navigation.navigate('EcranDeFinDePartie',{
-            gagne: true
-          })
-        }
-        />
-        <Button
-        title="Perdre"
-        onPress={() =>
-          navigation.navigate('EcranDeFinDePartie',{
-            gagne: false
-          })
-        }
-        />
+        <Text>En travaux</Text>
+        
         </View>
     )
 }
