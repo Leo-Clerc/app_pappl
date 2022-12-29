528f18cc9d76551287607b31e28b1ba5aa4c54e5
Leo-Clerc <114158809+Leo-Clerc@users.noreply.github.com>
2022-12-29 12:04:20 +0100

    Boucle for réparée, strucute pour créer la liste et l'afficher créée

--- EcranDeJeu2.js
+++ EcranDeJeu2.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Text, View, Button } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
+const nombreDeDonneurs = 4;
 const nomDonneurCorrect = "Dr Maya Curado";
 const ageDonneurCorrect = 36;
 const donneursPotentiels = [["Léon Patounec", 22, "M"],["Dr Emilia Caubert",65, "F"],["Thomas Parker",61, "M"],["Béatrice Tapalinga",23, "F"],["Vlad Koscov",55,"M"]];
@@ -13,13 +14,13 @@ function getRandomInt(max) {
 const positionDonneurCorrect = getRandomInt(4);
 
 let indexFauxDonneurs = [];
-while(indexFauxDonneurs.length<3){
+while(indexFauxDonneurs.length<nombreDeDonneurs-1){
   var nouvelIndex = getRandomInt(5)
   if(indexFauxDonneurs.indexOf(nouvelIndex)===-1){
     indexFauxDonneurs.push(nouvelIndex)
   }
 }
-const Donneurs =  [];
+/*const Donneurs =  [];
 let donneurCorrectAjoute = false;
 for(index=0;index<indexFauxDonneurs.length+1;index++){
   if(index==positionDonneurCorrect){
@@ -30,13 +31,39 @@ for(index=0;index<indexFauxDonneurs.length+1;index++){
   Donneurs.push(donneursPotentiels[indexFauxDonneurs[index]])
   }
   else{Donneurs.push(donneursPotentiels[indexFauxDonneurs[index-1]])}
-}
+}*/
 
 const EcranDeJeu2 = ({ navigation }) => {
-    return(
+  ages = []
+  changeAges = []
+  changeAgesOk = []
+  genres=[]
+  changeGenres = []
+
+  for(index=0;index<nombreDeDonneurs;index++){
+    [age,setAge] = useState(20);
+    const changeAge = (value) => setAge(value)
+    ages.push(age)
+    changeAges.push(changeAge)
+  }
+  Donneurs = []
+  let donneurCorrectAjoute = false;
+for(let index=0;index<nombreDeDonneurs;index++){
+  dernierStop = index;
+  if(index==positionDonneurCorrect){
+    Donneurs.push(<Text key={index.toString()}>Test, et c'est le bon : {index}</Text>)
+    donneurCorrectAjoute = true;
+  }
+  else if(!donneurCorrectAjoute){
+  Donneurs.push(<Text key={index.toString()}>Test {index+1}</Text>)
+  }
+  else{Donneurs.push(<Text key={index.toString()}>Test {index}+2</Text>)
+}}  
+ 
+  return(
         <View>
-        <Text>En travaux</Text>
-        
+        {Donneurs}
+        <Text>{dernierStop}</Text>
         </View>
     )
 }
