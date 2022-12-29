7faf2cd8f608cfb4597ff0320dfbe0e62d1b254e
Leo-Clerc <114158809+Leo-Clerc@users.noreply.github.com>
2022-12-29 18:53:40 +0100

    Les boutons s'affichent maintenant quand tout est rempli

--- EcranDeJeu2.js
+++ EcranDeJeu2.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Text, View, Button } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import Donneur from './Donneur'
@@ -41,7 +41,9 @@ const EcranDeJeu2 = ({ navigation }) => {
   const changeAgesOk = []
   const genres=[]
   const changeGenres = []
-
+  let [resolu,setResolu] = useState(false);
+  const verifierTout = () => {if(genres.every(Boolean)&&agesOk.every(Boolean)){setResolu(resolu=true)}};
+  useEffect(() => verifierTout());
   for(index=0;index<nombreDeDonneurs;index++){
     const [age,setAge] = useState(20);
     ages.push(age)
@@ -49,33 +51,52 @@ const EcranDeJeu2 = ({ navigation }) => {
     changeAges.push(changeAge)
     const [ageOk,setAgeOk] = useState(false)
     agesOk.push(ageOk)
-    const changeAgeOk = (value) => setAgeOk(value)
+    const changeAgeOk = (value) => {setAgeOk(value);}
     changeAgesOk.push(changeAgeOk)
     const [genre,setGenre] = useState();
     genres.push(genre)
-    const changeGenre = (value) => setGenre(value)
+    const changeGenre = (value) => {setGenre(value);}
     changeGenres.push(changeGenre)
   }
-  Donneurs = []
+  const Donneurs = []
   let donneurCorrectAjoute = false;
 for(let index=0;index<nombreDeDonneurs;index++){
   dernierStop = index;
   if(index==positionDonneurCorrect){
     Donneurs.push(<Donneur key={index.toString()} nom = {nomDonneurCorrect} age = {ages[index]} bonAge = {ageDonneurCorrect} 
     changeAge = {changeAges[index]} ageOk = {agesOk[index]} changeAgeOk = {changeAgesOk[index]} indicationGenre = "F" genre = {genres[index]} changeGenre={changeGenres[index]} compatibilite= {100}
+    correct = {true} resolu = {resolu} 
     />)
     donneurCorrectAjoute = true;
   }
   else if(!donneurCorrectAjoute){
-  Donneurs.push(<Text key={index.toString()}>Test {index+1}</Text>)
+  Donneurs.push(<Donneur key={index.toString()} nom = {donneursPotentiels[indexFauxDonneurs[index]][0]} 
+  age = {ages[index]} 
+  bonAge = {donneursPotentiels[indexFauxDonneurs[index]][1]} 
+  changeAge = {changeAges[index]} ageOk = {agesOk[index]} changeAgeOk = {changeAgesOk[index]} 
+  indicationGenre = {donneursPotentiels[indexFauxDonneurs[index]][2]}
+  genre = {genres[index]} 
+  changeGenre={changeGenres[index]} 
+  compatibilite= {donneursPotentiels[indexFauxDonneurs[index]][3]}
+  correct = {false} resolu = {resolu}
+  />)
   }
-  else{Donneurs.push(<Text key={index.toString()}>Test {index}+2</Text>)
+  else{Donneurs.push(<Donneur key={index.toString()} nom = {donneursPotentiels[indexFauxDonneurs[index-1]][0]} 
+  age = {ages[index]} 
+  bonAge = {donneursPotentiels[indexFauxDonneurs[index-1]][1]} 
+  changeAge = {changeAges[index]} ageOk = {agesOk[index]} changeAgeOk = {changeAgesOk[index]} 
+  indicationGenre = {donneursPotentiels[indexFauxDonneurs[index-1]][2]}
+  genre = {genres[index]} 
+  changeGenre={changeGenres[index]} 
+  compatibilite= {donneursPotentiels[indexFauxDonneurs[index-1]][3]}
+  correct = {false} resolu = {resolu}
+  />)
 }}  
  
   return(
         <View>
+        <Text>{resolu}</Text>
         {Donneurs}
-        <Text>{dernierStop}</Text>
         </View>
     )
 }
--- donneur.js
+++ donneur.js
@@ -5,14 +5,18 @@ import React from 'react';
 import { View } from "react-native";
 import ChampAge from './ChampAge';
 
-const Donneur = ({nom, age , bonAge, imageSource, ageOk, changeAge, changeAgeOk, indicationGenre , genre, changeGenre, compatibilite}) => {
+const Donneur = ({nom, age , bonAge, imageSource, ageOk, changeAge, changeAgeOk, indicationGenre , genre, changeGenre, compatibilite, 
+    correct, resolu                    
+}) => {
     return <View>
             
             <Text>{nom}</Text>
             <Button title="Homme" onPress={() => changeGenre(indicationGenre==="M")}/>
             <Button title="Femme" onPress={() => changeGenre(indicationGenre==="F")}/>
             <ChampAge bonAge={bonAge} age = {age} changeAge={changeAge} changeAgeOk = {changeAgeOk}/>
-            <>{{ageOk}&&{genre}&&<Text>Compatibilité : {compatibilite}</Text>}</>
+            <>{ageOk&&genre&&<Text>Compatibilité : {compatibilite}%</Text>}</>
+            <>{resolu&&<Button title={correct ? "gagner": "perdre"}/>}</>
+            <Text>{resolu}</Text>
     </View>
 }
 
